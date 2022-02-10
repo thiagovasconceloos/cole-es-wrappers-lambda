52cebcc10962b68106bd4480f8446a9678d61265
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-10 16:15:47 -0300

    Arrays

---
+++ src/br/com/bytebank/banco/testes/TesteArray.java
@@ -0,0 +1,32 @@
+package br.com.bytebank.banco.testes;
+
+
+public class TesteArray {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           
+		
+		int[] idades = new int[5];
+		
+		
+		
+	  for(int i = 0 ; i < idades.length; i++) {
+		  
+		  idades[i] = i * i;
+		  
+		  System.out.println(idades[i]);
+		  
+		  
+		  
+	  }
+		
+		
+		
+		
+	}
+	
+	
+	
+}
