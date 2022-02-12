fedf9bc586b8c009fbc59cfb29864a43913a5a04
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-12 13:52:18 -0300

    Autoboxing & Unboxing

---
+++ src/br/com/bytebank/banco/testes/util/Teste.java
@@ -0,0 +1,55 @@
+package br.com.bytebank.banco.testes.util;
+
+import java.util.*;
+
+public class Teste {
+	
+	public static void main(String[] args) { 
+		
+		
+		int[] idades = new int[5];
+		String[] nomes = new String[5];
+		
+		int idade = 29 ;
+		
+		Integer idadeRef = Integer.valueOf(29); //autoboxing 
+		
+		
+		System.out.println(idadeRef.doubleValue());
+
+
+		
+	
+		
+		
+		int valor = idadeRef.intValue(); 
+		
+		String s  = args[0]; // 10
+		
+		
+		//Integer numero  = Integer.valueOf(s);
+		 
+		Integer numero = Integer.parseInt(s);
+	    
+		System.out.println(numero);
+		List<Integer> numeros = new ArrayList<Integer>();
+		numeros.add(29);
+		
+		
+	
+				
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
